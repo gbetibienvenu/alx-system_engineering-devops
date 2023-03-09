1e937ff239122e261228999593800bd58767d0ab
gbetibienvenu <64703165+gbetibienvenu@users.noreply.github.com>
2023-03-09 09:40:11 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,6 @@
-# session failure (too many open files)
-exec {'hard':
-path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
-command  => "sudo sed -i 's/holberton hard nofile 5/holberton hard nofile 65536/g' /etc/security/limits.conf; /sbin/sysctl -p",
-provider => 'shell',
-}
-exec {'soft':
-path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
-command  => "sudo sed -i 's/holberton soft nofile 4/holberton soft nofile 65536/g' /etc/security/limits.conf; /sbin/sysctl -p",
-provider => 'shell',
+# fix our stack so that there is not failed requests
+
+exec { 'change nginx limit':
+    command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
+    provider => shell,
 }
